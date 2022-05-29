5d92f790cb87b0b66be4b9d24d240b5ce0461792
karateKime <iqbaljka@gmail.com>
2022-05-29 16:17:38 +0700

    ini adalah commit pertama saya

---
+++ dasar_javaScript.js
@@ -0,0 +1,13 @@
+const x = 3.14;
+
+console.log('SELAMAT SIANG')
+console.log(x);
+
+// coba variable pakai let
+
+let apelPerKilo = 10000;
+let jerukPerKilo = 15000;
+
+let totalBelanja = 2*apelPerKilo + 3*jerukPerKilo;
+
+console.log(totalBelanja);
